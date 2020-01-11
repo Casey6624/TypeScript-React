f9c301c22dfa428a6c6f9803e4056d57f1d2d7c2
Casey6624 <34159052+Casey6624@users.noreply.github.com>
2020-01-11 15:17:24 +0000

    useRef

--- src/Components/TextField.tsx
+++ src/Components/TextField.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState. useRef } from "react";
 
 interface Person {
   firstName: string;
@@ -13,13 +13,22 @@ interface Props {
   fn2: () => string; // function that returns a string
   fn3: (name: string) => string; // function which takes a string argument and returns another string
   person: Person; // person which grabs data from the above interface
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 // FC = FunctionComponent, TypeScript knows this is a functional component
 // export const TextField: React.FC<{text: string}> = () => { <---- this is also valid, by interface is neater
-export const TextField: React.FC<Props> = () => {
+export const TextField: React.FC<Props> = (handleChange) => {
+  // useState data is automatically typed for you, || const [count, setCount] = useState(5); || count would be number
+  /* We don't have to speicify a return type for useState, TypeScript will auto detect this value.
+    However if we needed a value to be null or a number then we can explicitly set this using the notation below
+    */
+  const [count, setCount] = useState<number | null>(5);
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
   return (
     <div>
-      <input />
+      <input ref={inputRef} onChange={handleChange}/>
     </div>
   );
 };
